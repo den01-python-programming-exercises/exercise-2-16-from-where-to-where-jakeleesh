ca11084e8c584e87c11d572b89dd5f0c85c2a5c7
Jake Lee <jake.lee@fdmgroup.com>
2021-09-02 21:38:50 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    end = int(input("Where to?"))
+    start = int(input("Where from?"))
+    for i in range(start,end + 1):
+        print(i)
 
 if __name__ == '__main__':
     main()
